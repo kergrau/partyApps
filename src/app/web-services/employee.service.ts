6ecfd3152d49733bfd9f3f91d37be36bef1b6063
kergrau <kergrau@hotmail.com>
2020-03-24 14:19:11 -0500

    Handle 401 and 403 errors in employee

--- src/app/web-services/employee.service.ts
+++ src/app/web-services/employee.service.ts
@@ -1,36 +1,54 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Employee } from '../employees/employee';
 import { Router } from '@angular/router';
 import { map, catchError, tap } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router,
+    private authService: AuthService) { }
 
   private BaseUrl = "http://localhost:9090/employee";
+    private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
   private isUnauthorized(error): boolean{
-    if(error.status == 401 || error.status == 403 ){
+    if(error.status == 401){
       this.router.navigate(['/login']);
+      return true;
+    }
+
+    if(error.status == 403){
+      alert("Denied Access")
+      //this.router.navigate(['/login']);
       return true;
     }
     return false;
   }
 
+  private addAuthorization(){
+    let token = this.authService.token;
+    if(token != null){
+      return this.httpHeaders.append('Authorization', 'Bearer ' + token);
+    }
+    return this.httpHeaders;
+  }
+
   getListAll(): Observable<Employee[]>{
-    return this.http.get<Employee[]>(this.BaseUrl+"/listAll")
-    .pipe(catchError(error => {
+    return this.http.get<Employee[]>(this.BaseUrl+"/listAll",
+     {headers: this.addAuthorization()}).pipe(catchError(error => {
       this.isUnauthorized(error);
       return throwError(error);
     } ));
   }
   createEmployee(employee: Employee): Observable<Employee>{
-    return this.http.post<Employee>(this.BaseUrl+"/create", employee).pipe(
+    return this.http.post<Employee>(this.BaseUrl+"/create", employee,
+      {headers: this.addAuthorization()}).pipe(
       catchError(
         error => {
           if (this.isUnauthorized(error)){
@@ -42,7 +60,8 @@ export class EmployeeService {
       
   }
   deleteEmployee(id): Observable<any>{
-    return this.http.delete<Employee>(this.BaseUrl+"/delete/"+id).pipe(
+    return this.http.delete<Employee>(this.BaseUrl+"/delete/"+id,
+      {headers: this.addAuthorization()}).pipe(
       catchError(
         error => {
           if (this.isUnauthorized(error)){
@@ -53,7 +72,8 @@ export class EmployeeService {
     );
   }
   updateEmployee(employee): Observable<Employee>{
-    return this.http.put<Employee>(this.BaseUrl+"/edit", employee).pipe(
+    return this.http.put<Employee>(this.BaseUrl+"/edit", employee,
+    {headers: this.addAuthorization()}).pipe(
       catchError(
         error => {
           if (this.isUnauthorized(error)){
